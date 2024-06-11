a0d891ea6c9062a6ffc837f2b764ce5399b9a0dd
Vivek Mishra <vivekmishra172003@gmail.com>
2024-06-11 19:19:25 +0530

    just started recursion

---
+++ Recursion/Q1 Print sum of n natural number/Check.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Check{
+    public static int sum(int n){
+        if(n=)
+    }
+    public static void main(String[] args){
+        System.out.println("Cehc");
+        int t = sum(n);
+    }
+}
+//sum of n natural number
