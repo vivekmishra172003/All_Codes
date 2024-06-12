745e578851c18ba9a4f25365f3943458c2a61e3a
Vivek Mishra <vivekmishra172003@gmail.com>
2024-06-12 12:20:40 +0530

    hashing started

---
+++ hash/hashmap_Operations/Check.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Check{
+    public static void main(String[] args){
+        System.out.println("Check");
+        HashMap<String,Integer> hm = new HashMap<>();
+        hm.put()
+    }
+}
