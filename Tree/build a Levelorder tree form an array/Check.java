14e06676cf856cb59d29943228d698c585b89d4b
Vivek Mishra <vivekmishra172003@gmail.com>
2024-06-14 19:08:24 +0530

    tommor i will complete build tree from an array

--- Tree/build a Levelorder tree form an array/Check.java
+++ Tree/build a Levelorder tree form an array/Check.java
@@ -1,42 +1,43 @@
 import java.util.*;
+public class Check{
+    public static TreeNode constructBinaryTree(int[] arr){
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int i =1;
+        while(!q.isEmpty() && i<arr.length){
+            int current = q.poll();
+            if(i<arr.length && arr[i] != -1){
+                current.left = new TreeNode(arr[i]);
+                q.offer(current.left);
+            }
+            i++;
+            if(i<arr.length && arr[i] != -1){
+                current.right= new TreeNode(arr[i]);
+                q.offer(current.right);
+                i++;
+            }
+            return root;
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("Check the shit");
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        TreeNode root = constructBinaryTree(arr);
+        System.out.println(root.data);
+    }
+}
+
 class TreeNode{
-    int val;
-    TreeNode left,right;
-    public TreeNode(int val){
-        this.val = val;
+    int data;
+    int left;
+    int right;
+    public TreeNode(int data){
+        this.data = data;
         this.left = null;
         this.right = null;
-    }
-}
-public static TreeNode constructBinaryTree(int[] arr){
-    if(arr=null || arr.length ==0){
-        return null;
-    }
-    TreeNode root = new TreeNode(arr[0]);
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.offer(root);
-    int i =1;
-    while(!queue.isEmpty() && i<arr.length){
-        TreeNode current = queue.poll();
-        if(i<arr.length && arr[i] != -1){
-            current.left = new TreeNode(arr[i]);
-            queue.offer(current.left);
-        }
-        i++;
-        if(i<arr.length && arr[i] != -1){
-            current.right = new TreeNode(arr[i]);
-            queue.offer(current.right);
-        }
-        i++;
-    }
-    return root;
-}
-public class Check{
-    public static void main(String[] args){
-        int[] arr = {1,2,3,4,5,6,7};
-        TreeNode root = constructBinaryTree(arr);
-                // Output the constructed tree using level order traversal
-        System.out.print("Level order traversal of the constructed tree: ");
-        levelOrderTraversal(root);
     }
 }
