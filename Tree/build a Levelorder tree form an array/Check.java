b44d46d101f4a9c3e2cd0c50fe2fca33fc0fe881
vivekmishra172003 <vivekmishra172003@gmail.com>
2024-06-18 19:41:05 +0530

    successfully build a tree from an array in level order fashion

--- Tree/build a Levelorder tree form an array/Check.java
+++ Tree/build a Levelorder tree form an array/Check.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Check{
     public static TreeNode constructBinaryTree(int[] arr){
-        if (arr == null || arr.length == 0) {
+        if(arr == null || arr.length ==0){
             return null;
         }
         TreeNode root = new TreeNode(arr[0]);
@@ -9,34 +9,32 @@ public class Check{
         q.offer(root);
         int i =1;
         while(!q.isEmpty() && i<arr.length){
-            int current = q.poll();
+            TreeNode current = q.poll();
             if(i<arr.length && arr[i] != -1){
                 current.left = new TreeNode(arr[i]);
                 q.offer(current.left);
             }
             i++;
-            if(i<arr.length && arr[i] != -1){
-                current.right= new TreeNode(arr[i]);
-                q.offer(current.right);
-                i++;
+            if(i<arr.length && arr[i]!= -1){
+                current.right = new TreeNode(arr[i]);
+                q.add(current.right);
             }
-            return root;
+            i++;
         }
+        return root;
     }
     public static void main(String[] args){
         System.out.println("Check the shit");
-        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int[] arr = {1,2,3,4,5,6,7};
         TreeNode root = constructBinaryTree(arr);
-        System.out.println(root.data);
+        System.out.println(root.val);
     }
 }
-
 class TreeNode{
-    int data;
-    int left;
-    int right;
-    public TreeNode(int data){
-        this.data = data;
+    int val;
+    TreeNode left,right;
+    public TreeNode(int val){
+        this.val = val;
         this.left = null;
         this.right = null;
     }
