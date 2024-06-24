24186c373c7858ce9bc06e4c0278539e850cdd46
vivekmishra172003 <vivekmishra172003@gmail.com>
2024-06-24 15:14:13 +0530

    Build adjmatrix and applied BFS

---
+++ Graph/Matrix/buildadjmatrix/Check.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Check {
+    private int adjMatrix[][];
+    int numVertices;
+
+    // Constructor to initialize the graph with the given number of vertices
+    Check(int numVertices) {
+        this.numVertices = numVertices;
+        adjMatrix = new int[numVertices][numVertices];
+    }
+
+    // Method to add an edge between two vertices in the graph
+    public void addEdge(int src, int dest) {
+        adjMatrix[src][dest] = 1;
+        adjMatrix[dest][src] = 1;
+    }
+
+    // Method to print the adjacency matrix of the graph
+    public void printAdjMatrix() {
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                System.out.print(adjMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Method to perform BFS traversal for all components of the graph
+    public void BFS() { // Removed static keyword
+        boolean[] visited = new boolean[numVertices];
+        for (int i = 0; i < numVertices; i++) {
+            if (!visited[i]) {
+                BFSUtil(i, visited);
+            }
+        }
+    }
+
+    // Utility method to perform BFS traversal from a given start vertex
+    public void BFSUtil(int start, boolean[] visited) { // Removed static keyword
+        Queue<Integer> q = new LinkedList<>();
+        visited[start] = true;
+        q.offer(start);
+        while (!q.isEmpty()) {
+            int vertex = q.poll();
+            System.out.print(vertex + " ");
+            for (int i = 0; i < numVertices; i++) {
+                if (adjMatrix[vertex][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    q.offer(i);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Check");
+        int numVertices = 7;
+        Check graph = new Check(numVertices);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(3, 4);
+        graph.addEdge(3, 5);
+        graph.addEdge(4, 5);
+        graph.addEdge(5, 6);
+        graph.addEdge(4, 2);
+        graph.printAdjMatrix();
+        
+        System.out.println("BFS Traversal:");
+        graph.BFS(); // Removed static keyword
+    }
+}
