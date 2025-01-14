380a5836ec819625bbc517ca2afe7f7a68448e3d
vivekmishra172003 <vivekmishra172003@gmail.com>
2025-01-14 09:49:54 +0530

    half of the preorder is done

---
+++ 2025/Q2 Build a tree from preorder/BinaryTree.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class BinaryTree{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node (int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class BinaryTreePre{
+        int index = -1;
+        public static Node buildTree(int nodes[]){
+            index++;
+            
+        }
+    }
+    }
+    public static void main(String[] args){
+        System.out.println("Check this is just sshit");
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        
+    }
+}
