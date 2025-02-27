4fe9d6d8906219b2232f5a2f6aef3e63a722f51e
vivekmishra172003 <vivekmishra172003@gmail.com>
2025-02-27 13:53:22 +0530

    Add SpringBoot project files

---
+++ SpringBoot/springBootProject01/src/main/java/com/springBootProject01/demo/SpringBootProject01Application.java
@@ -0,0 +1,13 @@
+package com.springBootProject01.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject01Application.class, args);
+	}
+
+}
---
+++ SpringBoot/springBootProject01/src/test/java/com/springBootProject01/demo/SpringBootProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootProject01.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
