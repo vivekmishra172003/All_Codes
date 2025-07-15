d9e11f6a45c7df4510f111ab05139fac745e47e1
vivekmishra172003 <vivekmishra172003@gmail.com>
2025-07-16 00:04:14 +0530

    now i have LLQuesion

---
+++ allcodes/LL/Q1 Build a LL/LL.java
@@ -0,0 +1,14 @@
+class LL{
+    public class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("Ram ram");
+    }
+}
