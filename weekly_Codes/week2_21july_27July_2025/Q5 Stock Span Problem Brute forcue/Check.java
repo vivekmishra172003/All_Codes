7658b32a2d567aa6b4f1890c6bfaf8f7b11cf6a1
vivekmishra172003 <vivekmishra172003@gmail.com>
2025-07-23 15:48:54 +0530

    Now going good with week 2

---
+++ weekly_Codes/week2_21july_27July_2025/Q5 Stock Span Problem Brute forcue/Check.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    public ArrayList<Integer> calculateSpan(int[] arr) {
+        // write code here
+        int n = arr.length;
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int i =0;i<n;i++){
+            int curr = arr[i];
+            int k =0;
+            for(int j =i;j>=0;j--){
+
+                if(curr>=arr[j]){
+                    k=k+1;
+                }else{
+                    break;
+                }
+            }
+            al.add(k);
+        }
+        return al;
+    }
+}
